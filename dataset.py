758c065f2aa89eca069f4e18326430bd1bfacaec
Yiyang Zeng <yz3622@nyu.edu>
2019-11-27 16:48:51 -0500

    Make the script loading full data default

--- dataset.py
+++ dataset.py
@@ -61,8 +61,12 @@ class Drive360(object):
             self.target_std[k] = np.asarray(v, dtype=np.float32)
 
         self.front = self.config['front']
+        self.tomtom = config['tomtom']
         self.right_left = config['multi_camera']['right_left']
         self.rear = config['multi_camera']['rear']
+        self.here = self.config['here']['enabled']
+        self.here_number = self.config['here']['number']
+        self.here_frequency = self.config['here']['frequency']
         self.truncate_test = config['truncate']
 
         #### reading in dataframe from csv #####
@@ -71,9 +75,51 @@ class Drive360(object):
                                             'cameraRear': object,
                                             'cameraRight': object,
                                             'cameraLeft': object,
+                                            'here': object,
+                                            'tomtom': object,
+                                            'hereMmLatitude': np.float32,
+                                            'hereMmLongitude': np.float32,
+                                            'hereSpeedLimit': np.float32,
+                                            'hereSpeedLimit_2': np.float32,
+                                            'hereFreeFlowSpeed': np.float32,
+                                            'hereSignal': np.float32,
+                                            'hereYield': np.float32,
+                                            'herePedestrian': np.float32,
+                                            'hereIntersection': np.float32,
+                                            'hereMmIntersection': np.float32,
+                                            'hereSegmentExitHeading': np.float32,
+                                            'hereSegmentEntryHeading': np.float32,
+                                            'hereSegmentOthersHeading': object,
+                                            'hereCurvature': np.float32,
+                                            'hereCurrentHeading': np.float32,
+                                            'here1mHeading': np.float32,
+                                            'here5mHeading': np.float32,
+                                            'here10mHeading': np.float32,
+                                            'here20mHeading': np.float32,
+                                            'here50mHeading': np.float32,
+                                            'hereTurnNumber': np.float32,
                                             'canSpeed': np.float32,
                                             'canSteering': np.float32
                                             })
+        #### scaling curvature values to within approximately -1.0 to 1.0
+        self.dataframe['hereCurvature'] = self.dataframe['hereCurvature'].values * 20.0
+
+        #### filtering and fixing here data #####
+        for k in ['hereSignal', 'hereYield', 'herePedestrian', 'hereIntersection', 'hereMmIntersection']:
+            self.dataframe[k].fillna(-100.0, inplace=True)
+            self.dataframe.loc[self.dataframe[k] < 0.0, k] = -100.0
+            self.dataframe.loc[self.dataframe[k] > 100.0, k] = -100.0
+            self.dataframe[k] = self.dataframe[k] / 100.0
+
+        for k in ['hereSpeedLimit', 'hereFreeFlowSpeed', 'hereCurrentHeading',
+                  'here1mHeading', 'here5mHeading', 'here10mHeading',
+                  'here20mHeading', 'here50mHeading',
+                  'hereSegmentEntryHeading', 'hereSegmentExitHeading']:
+            self.dataframe[k] = self.dataframe[k].ffill().bfill()
+            if 'Heading' in k:
+                self.dataframe[k] = self.dataframe[k] / 180.0
+            else:
+                self.dataframe[k] = self.dataframe[k] / 120.0
 
         # Here we calculate the temporal offset for the starting indices of each chapter. As we cannot cross chapter
         # boundaries but would still like to obtain a temporal sequence of images, we cannot start at index 0 of each chapter
@@ -94,7 +140,8 @@ class Drive360(object):
         # Thus the sixth sample will consist of images:     [*****]
 
         self.sequence_length = self.history_number*self.history_frequency
-        max_temporal_history = self.sequence_length
+        self.here_sequence_length = self.here_frequency*self.here_number
+        max_temporal_history = max(self.sequence_length, self.here_sequence_length)
         self.indices = self.dataframe.groupby('chapter').apply(lambda x: x.iloc[max_temporal_history:]).index.droplevel(level=0).tolist()
 
         #### phase specific manipulation #####
@@ -171,15 +218,47 @@ class Drive360(object):
                                      std=config['image']['norm']['std'])
             ]),
             'test': transforms.Compose([
-                transforms.Resize((320, 180)),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=config['image']['norm']['mean'],
                                      std=config['image']['norm']['std'])
             ])}
+        tomtom_transforms = {
+            'train': transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.6841898743159287, 0.6942467588984359, 0.63074608385988107],
+                                     std=[0.054145950202715391, 0.056086449019928965, 0.080349866693504177])
+            ]),
+            'validation': transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.6841898743159287, 0.6942467588984359, 0.63074608385988107],
+                                     std=[0.054145950202715391, 0.056086449019928965, 0.080349866693504177])
+            ]),
+            'test': transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.6841898743159287, 0.6942467588984359, 0.63074608385988107],
+                                     std=[0.054145950202715391, 0.056086449019928965, 0.080349866693504177])
+            ])}
+        here_transforms = {
+            'train': transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.6841898743159287, 0.6942467588984359, 0.63074608385988107],
+                                     std=[0.054145950202715391, 0.056086449019928965, 0.080349866693504177])
+            ]),
+            'validation': transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.6841898743159287, 0.6942467588984359, 0.63074608385988107],
+                                     std=[0.054145950202715391, 0.056086449019928965, 0.080349866693504177])
+            ]),
+            'test': transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.6841898743159287, 0.6942467588984359, 0.63074608385988107],
+                                     std=[0.054145950202715391, 0.056086449019928965, 0.080349866693504177])
+            ])}
 
         self.imageFront_transform = front_transforms[phase]
         self.imageSides_transform = sides_transforms[phase]
-
+        self.tomtom_transform = tomtom_transforms[phase]
+        self.here_transform = here_transforms[phase]
 
     def __getitem__(self, index):
         inputs = {}
@@ -192,11 +271,40 @@ class Drive360(object):
             inputs['cameraFront'] = {}
             for row_idx, (_, row) in enumerate(rows.iterrows()):
                 inputs['cameraFront'][row_idx] = (self.imageFront_transform(Image.open(self.data_dir + row['cameraFront'])))
+        
+        
+        if self.tomtom:
+            inputs['tomtom'] = self.tomtom_transform(Image.open(self.data_dir + rows['tomtom'].iloc[0]))
         if self.right_left:
             inputs['cameraRight'] = self.imageSides_transform(Image.open(self.data_dir + rows['cameraRight'].iloc[0]))
             inputs['cameraLeft'] = self.imageSides_transform(Image.open(self.data_dir + rows['cameraLeft'].iloc[0]))
         if self.rear:
             inputs['cameraRear'] = self.imageSides_transform(Image.open(self.data_dir + rows['cameraRear'].iloc[0]))
+        if self.here:
+            inputs['here'] = self.here_transform(Image.open(self.data_dir + rows['here'].iloc[0]))
+        ##### getting here data #####
+        here_end = index - self.here_sequence_length
+        here_skip = int(-1 * self.here_frequency)
+        here_rows = self.dataframe.iloc[index:here_end:here_skip].reset_index(drop=True, inplace=False)
+        #### Road Feature Information #####
+        inputs['hereSpeedLimit'] = here_rows['hereSpeedLimit'].values
+        inputs['hereFreeFlowSpeed'] = here_rows['hereFreeFlowSpeed'].values
+        inputs['hereSignal'] = here_rows['hereSignal'].values
+        inputs['hereYield'] = here_rows['hereYield'].values
+        inputs['herePedestrian'] = here_rows['herePedestrian'].values
+        inputs['hereCurvature'] = here_rows['hereCurvature'].values
+        inputs['hereMmIntersection'] = here_rows['hereMmIntersection'].values
+        inputs['hereIntersection'] = here_rows['hereIntersection'].values
+        inputs['hereTurnNumber'] = here_rows['hereTurnNumber'].values
+        inputs['hereSegmentEntryHeading'] = here_rows['hereSegmentEntryHeading'].values
+        inputs['hereSegmentExitHeading'] = here_rows['hereSegmentExitHeading'].values
+        #### Routing Information #####
+        inputs['here1mHeading'] = here_rows['here1mHeading'].values
+        inputs['here5mHeading'] = here_rows['here5mHeading'].values
+        inputs['here10mHeading'] = here_rows['here10mHeading'].values
+        inputs['here20mHeading'] = here_rows['here20mHeading'].values
+        inputs['here50mHeading'] = here_rows['here50mHeading'].values
+
         labels['canSteering'] = self.dataframe['canSteering'].iloc[index]
         labels['canSpeed'] = self.dataframe['canSpeed'].iloc[index]
 
