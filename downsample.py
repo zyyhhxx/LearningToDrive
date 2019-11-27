482fd01c54e41c2af31557005dbed84eccdf9705
Yiyang Zeng <yz3622@nyu.edu>
2019-11-27 16:16:43 -0500

    Downsample different dataset to different sizes

--- downsample.py
+++ downsample.py
@@ -11,28 +11,39 @@ def resize_image(f_name, output_name, resize_shape=(90, 160)):
 def downsample(src_path, dst_path):
     if not os.path.exists(dst_path):
         os.makedirs(dst_path)
+
     for loc in os.listdir(src_path):
         loc_path = "/" + loc
         if not os.path.exists(dst_path + loc_path):
             os.makedirs(dst_path + loc_path)
-        for camera in os.listdir(src_path + loc_path):
-            #if camera == "go_pro_4":
-            camera_path = loc_path + "/" + camera
-            if not os.path.exists(dst_path + camera_path):
-                os.makedirs(dst_path + camera_path)
-            for image in os.listdir(src_path + camera_path):
-                image_path = camera_path + "/" + image
+        for category in os.listdir(src_path + loc_path):
+            if category != "here":
+                continue
+            category_path = loc_path + "/" + category
+            if not os.path.exists(dst_path + category_path):
+                os.makedirs(dst_path + category_path)
+            for image in os.listdir(src_path + category_path):
+                image_path = category_path + "/" + image
                 if not os.path.exists(dst_path + image_path):
                     os.makedirs(dst_path + image_path)
                 for chapter in os.listdir(src_path + image_path):
                     chapter_path = image_path + "/" + chapter
+                    counter = 1
                     if not os.path.exists(dst_path + chapter_path):
                         os.makedirs(dst_path + chapter_path)
                     for sample in os.listdir(src_path + chapter_path):
                         sample_path = chapter_path + "/" + sample
-                        if not os.path.exists(dst_path + sample_path):
-                            resize_image(src_path + sample_path, dst_path + sample_path)
+                        if counter >= 10:
+                            counter = 0
+                            if not os.path.exists(dst_path + sample_path):
+                                if category == "here":
+                                    resize_image(src_path + sample_path, dst_path + sample_path, (200, 216))
+                                if category == "tomtom":
+                                    resize_image(src_path + sample_path, dst_path + sample_path, (150, 216))
+                                else:
+                                    resize_image(src_path + sample_path, dst_path + sample_path, (90, 160))
+                        counter += 1
                     print("Finishes chapter:" + chapter_path)
         print("Finishes location:" + loc_path)
     print("All done!")
-downsample("./data/Sample1", "./data/Sample1_downsampled_full")
+downsample("F:/Drive360challenge_images/Drive360Images", "./data/full_downsampled")
